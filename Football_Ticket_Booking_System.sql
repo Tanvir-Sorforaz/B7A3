0488ea04ca5ad4818e20d1909e2dccacbc454297
Tanvir-Sorforaz <tanvirsf12345@gmail.com>
2026-06-15 17:32:51 +0600

    added Query 1

--- Football_Ticket_Booking_System.sql
+++ Football_Ticket_Booking_System.sql
@@ -62,3 +62,8 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 
 
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+SELECT match_id, fixture, base_ticket_price
+FROM Matches
+WHERE tournament_category = 'Champions League'
+  AND match_status = 'Available';
